5e1961b563f8e619ff3ecf355b3d589cee911c8b
Artur Sousa <126983187+xavierrreu@users.noreply.github.com>
2023-08-16 16:31:36 -0300

    Create Chap2_1.js

---
+++ Chap2_1.js
@@ -0,0 +1,12 @@
+// cria uma referência ao form e ao h3(onde será exibida a resposta)
+
+const frm = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+//cria um 'ouvinte' de evento, acionado quando o botão submit for clicado
+
+frm.addEventListener("submit", (e) => {
+    const nome = frm.inNome.value  //obtém o valor digitado no form
+    resp.innerText = `Olá ${nome}` //exibe a resposta do programa
+    e.preventDefault()             //evita o envio do form
+})
